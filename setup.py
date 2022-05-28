4797b0ec944440dbdc221ca971c4cfa134d8cec3
Jason Zhang <zjjc123@hotmail.com>
2022-05-27 22:42:37 -0700

    License

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
 from setuptools import find_packages, setup
 
+# python setup.py bdist_wheel
+
 setup(
-    name='guipy',
+    name='guipylib',
     packages=find_packages(),
     version='0.1.0',
-    description=['guipy', 'guipy.components'],
+    description='User Interface Library for pygame',
     author='Casey Culbertson, Jason Zhang',
     license='MIT',
     install_requires=['pygame'],
