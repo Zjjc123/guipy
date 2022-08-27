24a70e5bb54fc40a694e09f23a345bc98b2417fe
Jason Zhang <zjjc123@hotmail.com>
2022-08-26 23:30:17 -0700

    Skip modules/functions that start with "_" in autoapi

--- docs/conf.py
+++ docs/conf.py
@@ -6,25 +6,37 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'Guipy'
-copyright = '2022, Casey Culbertson, Jason Zhang'
-author = 'Casey Culbertson, Jason Zhang'
-release = '0.0.0'
+project = "Guipy"
+copyright = "2022, Casey Culbertson, Jason Zhang"
+author = "Casey Culbertson, Jason Zhang"
+release = "0.0.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['autoapi.extension']
+extensions = ["autoapi.extension"]
 
-autoapi_type = 'python'
-autoapi_dirs = ['../guipy']
+autoapi_type = "python"
+autoapi_dirs = ["../guipy"]
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+def skip_util_classes(app, what, name, obj, skip, options):
+    # if name starts with a _
+    if name.split(".")[-1][0] == "_":
+        skip = True
+    return skip
+
+
+def setup(sphinx):
+    sphinx.connect("autoapi-skip-member", skip_util_classes)
+
+
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
